2365934295912fe5f4b6db5b7d217a5dad0db3c0
Lokesh <Loky.ece@gmail.com>
2023-04-11 19:13:48 +0100

    dark mode

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const darkModeButton = document.querySelector('#dark-mode-button');
-const body = document.querySelector('body');
+// const darkModeButton = document.querySelector('#dark-mode-button');
+// const body = document.querySelector('body');
 
-darkModeButton.addEventListener('click', function() {
-  body.classList.toggle('dark-mode');
+// darkModeButton.addEventListener('click', function() {
+//   body.classList.toggle('dark-mode');
+// });
+const toggleButton = document.querySelector('#toggle');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
 });
